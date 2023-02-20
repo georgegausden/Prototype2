30c8a783b79f8a96d3e6d7a18f3cd488f35cefa2
georgegausden <george.philip.gausden@gmail.com>
2023-02-19 19:56:36 -0500

    prototype2

---
+++ Prototype 2/Assets/PackageReceived.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PackageReceived : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.gameObject.CompareTag("Package"))
+        {
+            
+
+            
+
+           
+
+           // this.GetComponent<AudioSource>().Play();
+
+            Debug.Log("received");
+        }
+    }
+}
